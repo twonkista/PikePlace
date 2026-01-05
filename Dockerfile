a154be8d66aef415c267f8593c5621e3198cdf26
twonkista <rkomati787@gmail.com>
2026-01-04 19:48:08 -0600

    cockerfile and endpoints

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
-FROM 
+# The build stage
+FROM golang:1.22 as builder
+WORKDIR /app
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o api cmd/api/*.go
+
+EXPOSE 8080
--- cmd/api/api.go
+++ cmd/api/api.go
@@ -18,6 +18,22 @@ func (app *application) mount() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+
+	// list all pools
+	// list open pools (or list pools with status=open)
+	// list resolved pools (or status=resolved)
+	// create a new pool
+	// place wager
+	// cancel wager
+	// list wagers for a user
+	// list results for a pool
+	// close pool (usually due to time expiration)
+	// resolve pools
+	// get pool by id
+	// get user by id
+	// create user
+	// create user (admin only)
+
 	return mux
 }
 
